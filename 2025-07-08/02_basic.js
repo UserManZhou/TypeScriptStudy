7111cfd0bffeb19471e454cadd8cefe678646cba
zh <2830634496@qq.com>
2025-07-08 21:56:14 +0800

    更新hello.js和hello.ts文件，分别输出'HELLO JS'和'HELLO TS'。

---
+++ 2025-07-08/02_basic.js
@@ -0,0 +1,12 @@
+var a;
+// 上面已经定义number不能赋值字符串
+// a = "1";
+a = 1;
+a = 2;
+var b;
+b = "HELLO";
+function sum(a, b) {
+    return a + b;
+}
+var result = sum(1, 2);
+console.log(result);
---
+++ 2025-07-08/02_basic.ts
@@ -0,0 +1,24 @@
+let a: number;
+
+// 上面已经定义number不能赋值字符串
+// a = "1";
+
+a = 1;
+a = 2;
+
+let b: string;
+b = "HELLO";
+
+let c: boolean;
+
+c = true;
+// c=123;
+
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+let result = sum(1, 2);
+
+console.log(result);
+
