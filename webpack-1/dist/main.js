665b8716c80e0d40b963be23df6d9ae6b641bdd7
zh <2830634496@qq.com>
2025-11-04 21:13:18 +0800

    refactor(webpack): 优化webpack配置并切换为生产模式
    - 将开发模式切换为生产模式以启用代码压缩
    - 添加环境变量配置禁用箭头函数
    - 优化babel-loader和ts-loader配置
    - 添加代码压缩优化选项
    - 简化生成的HTML和JS文件结构

--- webpack-1/dist/main.js
+++ webpack-1/dist/main.js
@@ -1,96 +1,20 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
+/******/ (function() { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/index.ts":
-/*!**********************!*\
-  !*** ./src/index.ts ***!
-  \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+;// ./src/m1.ts
+var m1 = "hello m1";
+;// ./src/index.ts
 
-eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _m1__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./m1 */ \"./src/m1.ts\");\n\nfunction add(a, b) {\n  return a + b;\n}\nvar obj = {\n  name: '张三'\n};\nconsole.log(add(1, 2));\nconsole.log('hello webpack');\nconsole.log(_m1__WEBPACK_IMPORTED_MODULE_0__.m1);\nconsole.log(obj);\n\n//# sourceURL=webpack://webpack-1/./src/index.ts?\n}");
-
-/***/ }),
-
-/***/ "./src/m1.ts":
-/*!*******************!*\
-  !*** ./src/m1.ts ***!
-  \*******************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   m1: () => (/* binding */ m1)\n/* harmony export */ });\nvar m1 = \"hello m1\";\n\n//# sourceURL=webpack://webpack-1/./src/m1.ts?\n}");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.ts");
-/******/ 	
+function add(a, b) {
+  return a + b;
+}
+var obj = {
+  name: '张三'
+};
+console.log(add(1, 2));
+console.log('hello webpack');
+console.log(m1);
+console.log(obj);
+console.log(Permissions);
 /******/ })()
 ;
--- webpack-1/webpack.config.js
+++ webpack-1/webpack.config.js
@@ -1,22 +1,29 @@
 //引入一个包
-const path = require("path");
+const path = require('path')
 //引入html-webpack-plugin插件
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 //引入clean-webpack-plugin插件
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { env } = require('process')
 
 // webpack 配置
 module.exports = {
-  // 设置模式为开发环境
-  mode: "development",
+  // 注意：设置为production模式会自动启用代码压缩
+  // 可以根据需要切换开发和生产模式
+  mode: 'production',
   //入口文件
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   //输出文件
   output: {
     // 输出文件的目录
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist'),
     // 输出文件的文件名
-    filename: "main.js",
+    filename: 'main.js',
+    //配置打包的环境变量
+    environment: {
+      // 配置箭头函数
+      arrowFunction: false,
+    },
   },
   //webpack打包时使用的模块
   module: {
@@ -27,32 +34,35 @@ module.exports = {
         test: /\.ts$/,
         //use 要使用的loader
         use: [
-        //  配置babel-loader
-        {
-          loader: "babel-loader",
-          // options 配置babel-loader的参数
-          options: {
-            // presets 配置预定义的环境
-            presets: [
-              // 指定要使用的环境
-              ["@babel/preset-env", 
-                {
-              // 配置要兼容的浏览器版本
-              targets: {
-                // 谷歌浏览器版本60及以上
-                chrome: "60",
-                // ie浏览器版本11及以上
-                ie: "11",
+          //  配置babel-loader
+          {
+            loader: 'babel-loader',
+            // options 配置babel-loader的参数
+            options: {
+              // presets 配置预定义的环境
+              presets: [
+                // 指定要使用的环境
+                [
+                  '@babel/preset-env',
+                  {
+                    // 配置要兼容的浏览器版本
+                    targets: {
+                      // 谷歌浏览器版本60及以上
+                      chrome: '60',
+                      // ie浏览器版本11及以上
+                      ie: '11',
+                    },
+                    //  配置useBuiltIns参数为usage
+                    useBuiltIns: 'usage',
+                    //  配置corejs参数为3
+                    corejs: 3,
                   },
-              //  配置useBuiltIns参数为usage
-              useBuiltIns: "usage",
-              //  配置corejs参数为3
-              corejs: 3,
-            }
-           ]
-          ],
+                ],
+              ],
+            },
           },
-        },"ts-loader" ],
+          'ts-loader',
+        ],
         //exclude 要排除的文件
         exclude: /node_modules/,
       },
@@ -65,7 +75,7 @@ module.exports = {
       //  配置html-webpack-plugin插件的参数
       //title: "这是一个webpack-1项目",
       //template 指定要使用的html模板文件
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     //  配置clean-webpack-plugin插件
     new CleanWebpackPlugin(),
@@ -73,6 +83,22 @@ module.exports = {
   //用来设置模块的解析规则
   resolve: {
     // 配置文件扩展名
-    extensions: [".ts", ".js"],
+    extensions: ['.ts', '.js'],
   },
-};
+  // 配置优化选项
+  optimization: {
+    // 最小化JS代码
+    minimize: true,
+    // 配置最小化工具
+    minimizer: [
+      // 使用webpack内置的TerserPlugin（webpack 5默认包含）
+      // 不需要额外安装terser-webpack-plugin
+      // 如果需要自定义配置，可以引入TerserPlugin并配置
+    ],
+  },
+}
+
+// 注意事项：
+// 1. 生产环境（mode: "production"）会自动启用代码压缩
+// 2. 开发环境（mode: "development"）可以通过minimize: true手动启用压缩
+// 3. 如果需要更详细的压缩配置，可以安装terser-webpack-plugin并进行配置
